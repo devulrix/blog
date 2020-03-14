3c50a02cbeb4de922513b7a08c0527fa7cfafb41
Ulrich Hoelscher <uhoelscher@ulrix-mac.local>
2020-03-14 20:57:55 +0100

    fix path error

--- ci/scripts/build-hugo.sh
+++ ci/scripts/build-hugo.sh
@@ -4,8 +4,6 @@ set -ue
 
 #unpack hugo release 
 tar zxf hugo-release/hugo_*_Linux-64bit.tar.gz 
-cp /hugo-release/hugo /usr/local/bin/hugo
+cp hugo-release/hugo /usr/local/bin/hugo
 
-pwd
-ls
 hugo -c blog/ -d ../blog-deployment
