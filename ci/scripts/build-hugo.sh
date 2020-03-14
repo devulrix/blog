3dc959c701de37f243f1e97e7a97edceb25a90bc
Ulrich Hoelscher <uhoelscher@ulrix-mac.local>
2020-03-14 20:08:08 +0100

    use fixed hugo release for build

---
+++ ci/scripts/build-hugo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ue
+
+#unpack hugo release 
+tar zxf hugo-release/hugo_*_Linux-64bit.tar.gz 
+cp hugo-release/hugo /usr/local/bin/hugo
+
+hugo -c blog/ -d ../blog-deployment
