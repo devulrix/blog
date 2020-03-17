b5677389f052d4a1a5683a87439592a6151bd88a
Ulrich Hoelscher <ulrich.hoelscher@outlook.com>
2020-03-17 09:55:56 +0100

    push to final destination

--- ci/scripts/ftp-push.sh
+++ ci/scripts/ftp-push.sh
@@ -6,4 +6,4 @@ set -ue
 
 ls blog-deployment
 
-lftp -c "open  -u $FTP_USERNAME,$FTP_PASSWORD $FTP_SERVER; mirror -R blog-deployment test"
+lftp -c "open  -u $FTP_USERNAME,$FTP_PASSWORD $FTP_SERVER; mirror -R blog-deployment blog"
